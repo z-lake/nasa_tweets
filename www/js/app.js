bad29a52e03bd4f914c7c0b350a1536e68be49a9
Z <zachary.austin.harris@gmail.com>
2015-09-06 22:32:09 -0500

    create AppCtrl

--- www/js/app.js
+++ www/js/app.js
@@ -17,3 +17,30 @@ angular.module('starter', ['ionic', 'ngCordova', 'ngTwitter'])
     }
   });
 })
+
+.controller('AppCtrl', function($scope, $ionicPlatform, $twitterApi, $cordovaOauth) {
+  var twitterKey = 'STORAGE.TWITTER.KEY';
+  var clientId = 'daLhFOdYvq0Jb4Uivdg5IL1hf';
+  var clientSecret = 'N28nLRPUjih4iWhuO1zaw6RbgtcIJ9aOVKPeDHS5jIw0CRLZa7';
+  var myToken = '';
+
+  $scope.tweet = {};
+
+  $ionicPlatform.ready(function() {
+    myToken = JSON.parse(window.localStorage.getItem(twitterKey));
+    if (myToken === '' || myToken === null) {
+      $cordovaOauth.twitter(clientId, clientSecret).then(function (success) {
+        myToken = success;
+        window.localStorage.setItem(twitterKey, JSON.stringify(success));
+        $twitterApi.configure(clientId, clientSecret, success);
+        $scope.showHomeTimeline();
+      }, function(error) {
+        console.log(error);
+      });
+    } else {
+      $twitterApi.configure(clientId, clientSecret, myToken);
+      $scope.showHomeTimeline();
+    }
+  });
+
+});
