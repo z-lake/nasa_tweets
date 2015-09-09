a0a92873f66405a87c9a8a55836ee80614b74f9a
Z <zachary.austin.harris@gmail.com>
2015-09-09 11:44:26 -0500

    break out controllers

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-angular.module('starter', ['ionic', 'ngCordova', 'ngTwitter'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova', 'ngTwitter'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -13,76 +13,5 @@ angular.module('starter', ['ionic', 'ngCordova', 'ngTwitter'])
   });
 })
 
-.controller('AppCtrl', function($scope, $ionicPlatform, $twitterApi, $cordovaOauth, $http) {
-  var twitterKey = 'STORAGE.TWITTER.KEY'; // storeOauthToken
-  var clientId = 'daLhFOdYvq0Jb4Uivdg5IL1hf';
-  var clientSecret = 'N28nLRPUjih4iWhuO1zaw6RbgtcIJ9aOVKPeDHS5jIw0CRLZa7';
-  var myToken = '';
-  var POTDApi = 'https://api.nasa.gov/planetary/apod?api_key=OKsZatPeSQtXGUtJ9DbrB2uxeGh6NQVqFZPCZVB2';
 
-  $scope.tweet = {};
 
-  $scope.nasa = {};
-
-  $ionicPlatform.ready(function() {
-    // check if there is a token stored in local storage
-    myToken = JSON.parse(window.localStorage.getItem(twitterKey));
-    if (myToken === '' || myToken === null) {
-      $cordovaOauth.twitter(clientId, clientSecret).then(function (success) {
-        myToken = success;
-        // set token for Twitter as a string in local storage
-        window.localStorage.setItem(twitterKey, JSON.stringify(success));
-        $twitterApi.configure(clientId, clientSecret, success);
-        $scope.getPOTD();
-      }, function(error) {
-        console.log(error);
-        $scope.getPOTD(); // Must call in error in order to view in browser
-      });
-    } else {
-      $twitterApi.configure(clientId, clientSecret, myToken);
-      $scope.getPOTD();
-    }
-  });
-
-  $scope.submitTweet = function() {
-    $twitterApi.postStatusUpdate($scope.tweet.message).then(function(result) {
-    });
-  }
-
-  $scope.correctTimestring = function(string) {
-    return new Date(Date.parse(string));
-  };
-
-  // default request to POTD api (today's date)
-  $scope.getPOTD = function() {
-    $http.get(POTDApi).
-    then(function(success) {
-      $scope.nasa = success.data;
-  }, function(error) {
-    console.log(error);
-  });
-  };
-
-  $scope.getPOTDByDate = function() {
-    var day = $scope.nasa.date.getDate();
-    var month = $scope.nasa.date.getMonth() + 1;
-    var year = $scope.nasa.date.getFullYear();
-    var newDate;
-    if(day < 10) {
-      day = '0'+ day;
-    }
-    if(month < 10) {
-      month = '0' + month;
-    }
-      newDate = year+'-'+month+'-'+day;
-      console.log(newDate);
-
-      // ajax request for new photo url
-    $http.get(POTDApi + '&date=' + newDate)
-    .then( function(success) {
-      $scope.nasa = success.data;
-    }, function(error) {
-    });
-  };
-
-});
