c56324b0912f930f0646cde7f87b866f2a8f1f87
Z <zachary.austin.harris@gmail.com>
2015-09-09 12:12:57 -0500

    successfully render template

--- www/js/app.js
+++ www/js/app.js
@@ -12,6 +12,36 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova', 'ngTwitt
     }
   });
 })
+.config(function($stateProvider, $urlRouterProvider) {
 
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.photo', {
+    url: '/photo',
+    views: {
+      'tab-photo': {
+        templateUrl: 'templates/tab-photo.html',
+        controller: 'AppCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/photo');
+
+});
 
 
